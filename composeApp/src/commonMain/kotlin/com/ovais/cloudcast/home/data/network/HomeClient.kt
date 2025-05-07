fdc4af166cf7765888afeb9c1b041545ae627a20
Syed Ovais Akhtar <ovaisakhtar@Ovais-Akhtar-MBP.local>
2025-05-07 17:25:11 +0500

    Fixed Harcoded Values

--- composeApp/src/commonMain/kotlin/com/ovais/cloudcast/home/data/network/HomeClient.kt
+++ composeApp/src/commonMain/kotlin/com/ovais/cloudcast/home/data/network/HomeClient.kt
@@ -19,21 +19,32 @@ class DefaultHomeClient(
     private val httpClient: HttpClient
 ) : HomeClient {
 
+    companion object {
+        private const val KEY_CITY = "q"
+        private const val KEY_AQI = "aqi"
+        private const val KEY_DAYS = "days"
+        private const val KEY_API_KEY = "key"
+        private const val YES = "yes"
+        private const val NO = "no"
+    }
+
     override suspend fun fetchWeatherInformation(
         city: String,
         canFetchAQI: Boolean
     ): Result<WeatherResponse, DataError.Remote> {
         return safeCall<WeatherResponse> {
-            httpClient.get("http://api.weatherapi.com/v1/$GET_WEATHER_FORECAST") {
-                parameter("key", "6f071b0321a74bbcb30152108252704")
-                parameter("q", city)
-                parameter("aqi", getAQIParams(canFetchAQI))
-                parameter("days", 7)
+            httpClient.get(GET_WEATHER_FORECAST) {
+                parameter(KEY_API_KEY, API_KEY)
+                parameter(KEY_CITY, city)
+                parameter(KEY_AQI, getAQIParams(canFetchAQI))
+                parameter(KEY_DAYS, FORECAST_DAY_HISTORY)
             }
         }
     }
 
-    private fun getAQIParams(isAQIEnabled: Boolean): String {
-        return if (isAQIEnabled) "yes" else "no"
+    private fun getAQIParams(
+        isAQIEnabled: Boolean
+    ): String {
+        return if (isAQIEnabled) YES else NO
     }
 }

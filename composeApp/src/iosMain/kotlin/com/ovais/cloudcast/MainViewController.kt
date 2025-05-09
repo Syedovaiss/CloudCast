c90f9eac4f41a899e6c196875bffa8966cf9284a
Syed Ovais Akhtar <ovaisakhtar@Ovais-Akhtar-MBP.local>
2025-05-09 19:31:18 +0500

    fixed ios app running issue

--- composeApp/src/iosMain/kotlin/com/ovais/cloudcast/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/ovais/cloudcast/MainViewController.kt
@@ -2,5 +2,10 @@ package com.ovais.cloudcast
 
 import androidx.compose.ui.window.ComposeUIViewController
 import com.ovais.cloudcast.core.presentation.app.CloudCast
+import com.ovais.cloudcast.di.initKoin
 
-fun MainViewController() = ComposeUIViewController { CloudCast() }
+fun MainViewController() = ComposeUIViewController(
+    configure = {
+        initKoin()
+    }
+) { CloudCast() }
